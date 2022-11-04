87e9267c4857886513626893bee55d26427e260d
dariusv2307 <vesadarius23072002@yahoo.com>
2022-11-04 20:01:42 +0200

    mutari

--- Hanoi.cpp
+++ Hanoi.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
 using namespace std;
+int n;
+char a, b, c;
+void hanoi(int n, char a, char c, char b)
+{
+    if (n == 1)
+        cout << a << "->" << c << endl;
+    else
+    {
+        hanoi(n - 1, a, b, c);
+        cout << a << "->" << c << endl;
+        hanoi(n - 1, b, c, a);
+    }
+}
 int main()
 {
     cin >> n;
